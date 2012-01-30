61c7e2332e2538c86642139adca121401187eefd
javiergarciasanchez <javiergarciasanchez@456ed184-0a24-420a-85c2-6890e3e6309c>
2012-01-30 20:55:29 +0000

    Batch fixed Connection SQL working

--- Sudden Stop/src/suddenStop/DataCollector.java
+++ Sudden Stop/src/suddenStop/DataCollector.java
@@ -1,15 +1,16 @@
 package suddenStop;
 
-import static repast.simphony.essentials.RepastEssentials.GetParameter;
 import static repast.simphony.essentials.RepastEssentials.GetTickCount;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 import repast.simphony.context.Context;
-import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.environment.RunInfo;
 import repast.simphony.engine.environment.RunState;
 import repast.simphony.engine.schedule.ScheduledMethod;
-import repast.simphony.parameter.Schema;
 import repast.simphony.util.collections.IndexedIterable;
 import suddenStop.Firm.Decision;
 import suddenStop.Firm.State;
@@ -17,61 +18,72 @@ import static repast.simphony.engine.schedule.ScheduleParameters.*;
 
 public class DataCollector {
 
-	private Context<Object> context = null;
-	SupplyManager suppMan = null;
-
+	private int simID;
 	private Connection con;
-	private final String conStr = "jdbc:sqlserver://localhost;databaseName=Simulations;integratedSecurity=true;";
+	private Context<Object> context = null;
+	private SupplyManager suppMan = null;
+	private RunInfo runInfo;
 
-	private String Simulation = null;
+	public DataCollector(Connection con, int simID, Context<Object> context, SupplyManager sm) {
 
-	public DataCollector(Context<Object> context) {
+		this.con = con;
+		this.simID = simID;
 
 		this.context = context;
+		suppMan = sm;
+
 		context.add(this);
-
-		try {
-			con = DriverManager.getConnection(conStr);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		Simulation = getSimNum();
-		
-		saveSimStart();
+	
+		runInfo = RunState.getInstance().getRunInfo();
 
 	}
 
-	private void saveSimStart() {
-		saveSimulationData();
-		saveSimulationParams();
-	}
-
-	@ScheduledMethod(start = 1, priority = FIRST_PRIORITY)
-	public void saveEveryRun() {
-		saveGlobalParams();
-	}
 
 	@ScheduledMethod(start = 1, interval = 1, priority = LAST_PRIORITY)
 	public void saveEveryStep() {
-		saveMktData();
-		saveFirmsData();
+		int run = runInfo.getRunNumber();
+		double tick = GetTickCount();
+		saveMktData(run, tick);
+		saveFirmsData(run, tick);
+
 	}
 
-	private void saveFirmsData() {
-		IndexedIterable<Object> firms = context.getObjects(Firm.class);
+	private void saveMktData(int run, double tick) {
 
-		if (firms.size() > 0.0) {
-			for (Object f : firms) {
-				saveFirmData(((Firm) f));
+		String price = ((Double) suppMan.price).toString();
+
+		String sqlStr = "INSERT INTO [Market Data] VALUES (" + simID + ", "
+				+ run + ", " + tick + "," + price + ")";
+
+		Statement stmt = null;
+		try {
+			stmt = con.createStatement();
+			stmt.executeUpdate(sqlStr);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("Error trying to write to \"Market Data\" table.");
+			System.exit(-1);
+		} finally {
+			try {
+				stmt.close();
+			} catch (Throwable ignore) {
 			}
 		}
 	}
 
-	private void saveFirmData(Firm f) {
-		String runNum = ((Integer) RunState.getInstance().getRunInfo()
-				.getRunNumber()).toString();
-		String tick = ((Double) GetTickCount()).toString();
+	private void saveFirmsData(int run, double tick) {
+		IndexedIterable<Object> firms = context.getObjects(Firm.class);
+
+		if (firms.size() > 0.0) {
+			for (Object f : firms) {
+				saveFirmData(run, tick, (Firm) f);
+			}
+		}
+	}
+
+	private void saveFirmData(int run, double tick, Firm f) {
+
 		State cs = f.currentState;
 		Decision cd = f.currentDecision;
 
@@ -79,21 +91,34 @@ public class DataCollector {
 				+ "Simulation, [Run Number], Tick, [Firm ID], "
 				+ "Profit, quantity, rD, FirstUnitCost, "
 				+ "Born, Expon, VarCost, MedCost) VALUES ("
-				
-				+ Simulation + ", "
-				+ runNum + ", "
-				+ tick 	+ ", "
-				+ "'" + f.toString() + "', "
-				
-				+ cs.profit + ", "
-				+ cd.quantity + ", "
-				+ cd.rD + ", "
-				+ cs.firstUnitCost 	+ ", "
-				
-				+ f.born + ", "
-				+ f.expon + ", "
-				+ f.varCost + ", "
-				+ f.medCost + " )";
+
+				+ simID
+				+ ", "
+				+ run
+				+ ", "
+				+ tick
+				+ ", "
+				+ "'"
+				+ f.toString()
+				+ "', "
+
+				+ cs.profit
+				+ ", "
+				+ cd.quantity
+				+ ", "
+				+ cd.rD
+				+ ", "
+				+ cs.firstUnitCost
+				+ ", "
+
+				+ f.born
+				+ ", "
+				+ f.expon
+				+ ", "
+				+ f.varCost
+				+ ", "
+				+ f.medCost
+				+ " )";
 
 		Statement stmt = null;
 		try {
@@ -102,133 +127,15 @@ public class DataCollector {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+			System.err.println("Error trying to write to \"Individual Firms\" table.");
+			System.exit(-1);
 		} finally {
-			try {stmt.close();} catch (Throwable ignore) {
+			try {
+				stmt.close();
+			} catch (Throwable ignore) {
 			}
 		}
 
-	}
-
-	private String getSimNum() {
-		// Get next simulation number
-		String sqlStr = "SELECT MAX(SimulID) FROM Simulations";
-
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery(sqlStr);
-			Integer s;
-			if (rs.next()) {
-				s = rs.getInt(1) + 1;
-			} else {
-				s = 1;
-			}
-			return s.toString();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {stmt.close();} catch (Throwable ignore) {
-			}
-		}
-		return null;
-	}
-
-	private void saveSimulationData() {
-		String desc = (String) GetParameter("simDescription");
-
-		String sqlStr = "INSERT INTO Simulations (SimulID, Description) "
-				+ "VALUES (" + Simulation + ", '" + desc + "' )";
-
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-			stmt.executeUpdate(sqlStr);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {stmt.close();} catch (Throwable ignore) {
-			}		}
-
-	}
-
-	private void saveSimulationParams() {
-
-		String sqlStr = "INSERT INTO [Simulation Parameters] VALUES ("
-				+ Simulation + ", ? , ? )";
-
-		Schema schema = RunEnvironment.getInstance().getParameters()
-				.getSchema();
-
-		PreparedStatement pstmt = null;
-
-		try {
-			pstmt = con.prepareStatement(sqlStr);
-			for (String paramName : schema.parameterNames()) {
-				if (paramName == "simDescription") {
-					continue;
-				} else {
-					pstmt.setString(1, paramName);
-					pstmt.setString(2, GetParameter(paramName).toString());
-					pstmt.executeUpdate();
-				}
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {pstmt.close();} catch (Throwable ignore) {
-			}		}
-
-	}
-
-	private void saveGlobalParams() {
-
-		String runNum = ((Integer) RunState.getInstance().getRunInfo()
-				.getRunNumber()).toString();
-		double ssM = (Double) GetParameter("suddenStopMagnitude");
-		int ssS = (Integer) GetParameter("suddenStopStart");
-		int rndSeed = (Integer) GetParameter("randomSeed");
-		Double robCheck = null;
-
-		String sqlStr = "INSERT INTO [Global Parameters] VALUES (" + Simulation
-				+ ", " + runNum + ", " + ssM + "," + ssS + ", " + rndSeed
-				+ ", " + robCheck + ")";
-
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-			stmt.executeUpdate(sqlStr);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {stmt.close();} catch (Throwable ignore) {
-			}		}
-
-	}
-
-	private void saveMktData() {
-		String runNum = ((Integer) RunState.getInstance().getRunInfo()
-				.getRunNumber()).toString();
-		String tick = ((Double) GetTickCount()).toString();
-		String price = ((Double) suppMan.price).toString();
-
-		String sqlStr = "INSERT INTO [Market Data] VALUES (" + Simulation
-				+ ", " + runNum + ", " + tick + "," + price + ")";
-
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-			stmt.executeUpdate(sqlStr);
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {stmt.close();} catch (Throwable ignore) {
-			}
-		}
 	}
 
 }
