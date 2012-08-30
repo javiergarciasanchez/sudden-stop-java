1d377616c10ceb7951a2f17ec7bf8a42e2adbc53
javiergarciasanchez <javiergarciasanchez@456ed184-0a24-420a-85c2-6890e3e6309c>
2012-08-30 19:30:25 +0000

    After graphs simplification to be sent to reviewers

---
+++ Sudden Stop/src/suddenStop/Tim1.java
@@ -0,0 +1,7 @@
+package suddenStop;
+
+public class Tim1 extends Cohort {
+	public Tim1(Firm f) {
+		super(f);
+	}
+}
---
+++ Sudden Stop/src/suddenStop/Tim3.java
@@ -0,0 +1,7 @@
+package suddenStop;
+
+public class Tim3 extends Cohort {
+	public Tim3(Firm f) {
+		super(f);
+	}
+}
